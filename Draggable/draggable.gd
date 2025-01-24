c0f7153fde0c909b68782b7650612d7f883b6963
John Rais <raisjohn@ecs.vuw.ac.nz>
2025-01-25 09:52:08 +1300

    Throwable draggables

--- Draggable/draggable.gd
+++ Draggable/draggable.gd
@@ -1,6 +1,9 @@
 extends RigidBody2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
+var is_dragging = false
+var drag_offset = Vector2(0,0)
+var follow_speed = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,18 +12,34 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if is_dragging:
+		var gain_velocity = ((get_global_mouse_position() - drag_offset) - global_position) * follow_speed
+		var close_dampening = 1 if (distance_to_mouse() > radius()) else 1 - (radius() - distance_to_mouse())/radius()
+		linear_velocity = (gain_velocity * close_dampening)
+		
 	
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("left_click") and get_global_mouse_position().distance_to(global_position) < collision_shape_2d.shape.radius :
-		apply_impulse(Vector2(-300,200))
+	if event.is_action_pressed("left_click") and distance_to_mouse() < radius() :
+		clicked_input(event)
+		
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.is_released():
+			is_dragging = false
 
+
+func clicked_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			is_dragging = true
+			drag_offset = get_global_mouse_position() - global_position
+
+
+#region Physics
 func _physics_process(delta: float) -> void:
 	screen_border_collisions()
 	
-	
-
 func screen_border_collisions():
+	
 	var screen_size = get_viewport().get_visible_rect().size
 	#left side of screen
 	if(left() < 0): linear_velocity = Vector2(abs(linear_velocity.x),linear_velocity.y)
@@ -34,33 +53,19 @@ func screen_border_collisions():
 	#bottom side of screen
 	if(bottom() > screen_size.y): linear_velocity = Vector2((linear_velocity.x),-abs(linear_velocity.y))
 
-#HELPER
+#endregion
+
+#region Helper functions
+func distance_to_mouse() -> float:
+	return get_global_mouse_position().distance_to(global_position);
 func radius() -> float:
 	return collision_shape_2d.shape.radius;
 func left() -> float:
-	return global_position.x - collision_shape_2d.shape.radius
+	return global_position.x - radius()
 func right() -> float:
-	return global_position.x + collision_shape_2d.shape.radius
+	return global_position.x + radius()
 func top() -> float:
-	return global_position.y - collision_shape_2d.shape.radius
+	return global_position.y - radius()
 func bottom() -> float:
-	return global_position.y + collision_shape_2d.shape.radius
-#func _input(event: InputEvent) -> void:
-	#if event is InputEventMouseButton:
-		#if event.button_index == MOUSE_BUTTON_LEFT and event.is_released():
-			#is_dragging = false
-#
-#
-#func _gui_input(event: InputEvent) -> void:
-	#if event is InputEventMouseButton:
-		#if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			#is_dragging = true
-			#drag_offset = get_global_mouse_position() - global_position
-#
-#
-#func _process(_delta: float) -> void:
-	#if is_dragging:
-		#global_position = get_global_mouse_position() - drag_offset
-	#elif is_resizing:
-		#size.x = max(get_global_mouse_position().x - global_position.x, 150.0)
-		#size.y = max(get_global_mouse_position().y - global_position.y, 70.0)
+	return global_position.y + radius()
+#endregion
