66e5f38e48bd825d9cf714539cbe76889d8eefd3
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-01-26 12:05:31 +1300

    all of the phases data entered

---
+++ CustomResources/final.gd
@@ -0,0 +1,9 @@
+extends Phase
+
+
+func _init() -> void:
+	pass
+	#messages.append(Message.new("", Message.Senders.MASON, "he mentioned someone called june or smth- and said not to talk to them??", Message.Emojis., , , , ""))
+	#messages.append(Message.new("", Message.Senders.MASON, "was very cryptic tbh ", Message.Emojis., , , , ""))
+	#messages.append(Message.new("", Message.Senders.MASON, "are you ok? ", Message.Emojis., , , , ""))
+	#messages.append(Message.new("", Message.Senders.NICK, "Hey, I need to explain. Can we talk? ", Message.Emojis., , , , ""))
