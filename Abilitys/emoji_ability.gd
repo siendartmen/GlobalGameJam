c24a9f598d08e8ab54352b9687ba1178104bda68
John Rais <raisjohn@ecs.vuw.ac.nz>
2025-01-25 20:59:53 +1300

    Finished ability interaction

--- Abilitys/emoji_ability.gd
+++ Abilitys/emoji_ability.gd
@@ -4,7 +4,8 @@ extends BaseAbility
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super()
-	req_points = 1
+	req_points = 0
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,4 +14,4 @@ func _process(delta: float) -> void:
 func use_ability(text_bubble:TextBubble) :
 	super(text_bubble)
 	#Show widget that shows the trace
-	print("USE TRACE")
+	print("USE EMOJI")
