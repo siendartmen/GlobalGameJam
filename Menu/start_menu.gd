9a5e517c111e56448bd8e4b512f274f203d6eda6
Ben Scott <benjamin.scott3008@gmail.com>
2025-01-26 15:13:21 +1300

    added description in about section

--- Menu/start_menu.gd
+++ Menu/start_menu.gd
@@ -15,7 +15,7 @@ func _on_button_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	print("Info Button Pressed")
-	#get_tree().change_scene_to_file()
+	get_tree().change_scene_to_file("res://Menu/about_menu.tscn")
 	
 
 func _on_button_3_pressed() -> void:
